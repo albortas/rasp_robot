752425cba39706680c20e5d418b22741ae5695c4
albortas <garlad628@gmail.com>
2026-07-07 09:47:09 -0400

    Abort Controller OK

--- src/hardware/abort_controller.py
+++ src/hardware/abort_controller.py
@@ -5,19 +5,34 @@ class AbortController:
     def __init__(self):
         log.debug("Iniciando control relay")
         self.relay = LED(17, active_high=True)
+        self._state = False
         self.abort()
     
     def activate_servos(self):
+        self._state = True
         log.debug("Rele activado")
         self.relay.on()
     
     def abort(self):
+        self._state = False
         log.debug("Rele apagado")
         self.relay.off()
+    
+    def toggle(self):
+        if self._state:
+            self.abort()
+        else:
+            self.activate_servos()
         
 if __name__ == "__main__":
     import time
     led = AbortController()
-    led.activate_servos()
-    time.sleep(2)
-    led.abort()
+    try:
+        while True:
+            led.toggle()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Terminado por el usurio")
+    finally:
+        led.abort()
+        print("Saliendo..")
